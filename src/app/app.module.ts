43179ec22dcc78f28a9e9aaece001262a1c73e94
joseemiguel <josem860@hotmail.com>
2018-06-15 12:35:48 -0400

    Incluyendo menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BlogDiscusionesComponent } from './blog-discusiones/blog-discusiones.component';
 import { FormsModule }  from '@angular/forms';
 import { RegistroVecinoComponent } from './registro-vecino/registro-vecino.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogDiscusionesComponent,
-    RegistroVecinoComponent
+    RegistroVecinoComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
