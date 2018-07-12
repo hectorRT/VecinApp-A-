477a186fe607cb721a38fe30a3eb1806004b92ec
EstebanMenaUCNE <esteban_mena@ucne.edu.do>
2018-07-12 18:01:46 -0400

    Registro de discusiones enlazado con estados, tanto en .html como en el .ts

--- src/app/discusion-registro/discusion-registro.component.ts
+++ src/app/discusion-registro/discusion-registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DiscusionesService } from "../../Servicios/Discusiones-Service/discusiones.service";
 import { Discusion } from "../../Clases/Discusion";
+import { EstadoDiscusion } from '../../Clases/EstadoDiscusion';
 
 @Component({
   selector: 'app-discusion-registro',
@@ -10,20 +11,28 @@ import { Discusion } from "../../Clases/Discusion";
 export class DiscusionRegistroComponent implements OnInit {
 
   discusion: Discusion;
+  estados: Array<EstadoDiscusion> = [];
 
-  constructor(private discusinService: DiscusionesService) { }
+  constructor(private discusionService: DiscusionesService) {
+  }
+
+  fetchEstados() {
+    this.discusionService.getEstados().subscribe(estados => {
+      this.estados = estados;
+    });
+  }
 
   ngOnInit() {
     this.discusion = new Discusion();
-  }
+    this.fetchEstados();
+  }  
 
   Guardar(){
     this.discusion.DateModification = this.discusion.FechaCreacion;
     this.discusion.IdVecino = 1;
     this.discusion.ModifyBy = 1;
-    this.discusion.Estado = 0;
 
-    this.discusinService.addDiscusion(this.discusion).subscribe(res =>
+    this.discusionService.addDiscusion(this.discusion).subscribe(res =>
       {
         console.log(res);
         // this.discusion.IdDiscusion = res.data.insertId;
