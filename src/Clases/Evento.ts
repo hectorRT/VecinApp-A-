c0bcdfc68b48bd7b1083ffdeda41637fdc832628
EstebanMenaUCNE <esteban_mena@ucne.edu.do>
2018-07-14 02:21:06 -0400

    Agregué modelo Evento

---
+++ src/Clases/Evento.ts
@@ -0,0 +1,12 @@
+export interface Evento {
+
+    IdEvento?: number,
+    IdVecindario?: number,
+    Nombre?: string,
+    Descripcion?: string,
+    Lugar?: string,
+    HoraInicio?: Date,
+    HoraFin?: Date,
+    FechaCreacion?: Date
+    
+}
