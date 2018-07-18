1cabd71243343faa5da3b11351564319ee7bf778
robert0329 <robert_frias2@ucne.edu.do>
2018-07-18 16:54:54 -0700

    Cambios en el menu, appmodule y mas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {UserService} from '../user.service';
-import { VecinoService } from '../../Servicios/Vecino-Service/vecino.service';
-import { Vecino } from '../../Clases/Vecino';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from './../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,40 +9,32 @@ import { Vecino } from '../../Clases/Vecino';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
 
-  constructor(private router:Router, private vecino:VecinoService) { }
-
-  vecinosArray:Vecino;
-  vecinosFilter:Vecino;
-  vecinosClave:Array<Vecino>=[];
-  InputEmail:string='';
-  Password:string='';
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-  }
-  /*loginUser(e) {
-  	e.preventDefault();
-  	console.log(e);
-  	var username = e.target.elements[0].value;
-  	var password = e.target.elements[1].value;
-  	
-  	if(username == 'admin' && password == 'admin') {
-      this.user.setUserLoggedIn(username);
-  		this.router.navigate(['menu']);
-  	}
-  }*/
-
-  
-  buscarVecino()
-  {
-
-    this.vecino.getVecinoEmail(this.InputEmail).subscribe(email=>{
-      this.vecinosArray=email;
-      console.log(this.vecinosArray);
-      
+    this.form = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
-  
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
 
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value);
+    }
+    this.formSubmitAttempt = true;
+  }
 }
