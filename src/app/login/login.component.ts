f02a0015cb2241c73ca874157612185e7ff885fc
joseemiguel <josem860@hotmail.com>
2018-07-18 16:36:32 -0400

    Haciendo Pull
    Actualizacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,39 +11,39 @@ import { Vecino } from '../../Clases/Vecino';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router:Router, private vecino:VecinoService) { }
+  constructor(private router: Router, private vecino: VecinoService) { }
 
-  vecinosArray:Vecino;
-  vecinosFilter:Array<Vecino>=[];
-  vecinosClave:Array<Vecino>=[];
-  InputEmail:string='';
-  Password:string='';
+  vecinosArray: Vecino;
+  vecinosFilter: Array<Vecino> = [];
+  vecinosClave: Array<Vecino> = [];
+  InputEmail: string ='';
+  Password: string ='';
 
   ngOnInit() {
   }
-  /*loginUser(e) {
-  	e.preventDefault();
-  	console.log(e);
-  	var username = e.target.elements[0].value;
-  	var password = e.target.elements[1].value;
+  // loginUser(e) {
+  // 	e.preventDefault();
+  // 	console.log(e);
+  // 	var username = e.target.elements[0].value;
+  // 	var password = e.target.elements[1].value;
   	
-  	if(username == 'admin' && password == 'admin') {
-      this.user.setUserLoggedIn(username);
-  		this.router.navigate(['menu']);
-  	}
-  }*/
+  // 	if(username == 'admin' && password == 'admin') {
+  //     this.user.setUserLoggedIn(username);
+  // 		this.router.navigate(['menu']);
+  // 	}
+  // }
 
   
   buscarVecino()
   {
 
-    this.vecino.getVecinoEmail(this.InputEmail).subscribe(email=>{
-      this.vecinosArray=email;
+    this.vecino.getVecinoEmail(this.InputEmail).subscribe(email => {
+      this.vecinosArray = email;
       console.log(this.vecinosArray);
 
     });
   }
 
-  
+
 
 }
