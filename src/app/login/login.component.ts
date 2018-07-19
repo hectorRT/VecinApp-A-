aacd37d2f38eaf7752401be794a12a7797215f4f
joseemiguel <josem860@hotmail.com>
2018-07-19 13:59:19 -0400

    Omitiendo codigo duplicado.
    Los codigos duplicados fueron comentados.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
+import { VecinoService } from './../../Servicios/Vecino-Service/vecino.service';
+import { Vecino } from './../../Clases/Vecino';
+
+import { Route } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 import { AuthService } from './../auth/auth.service';
 
@@ -12,50 +17,47 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   private formSubmitAttempt: boolean;
 
-<<<<<<< HEAD
-  constructor(private router: Router, private vecino: VecinoService) { }
 
-<<<<<<< HEAD
+ // constructor(private router: Route, private vecino: VecinoService) {}
+
   vecinosArray: Vecino;
   vecinosFilter: Array<Vecino> = [];
   vecinosClave: Array<Vecino> = [];
-  InputEmail: string ='';
-  Password: string ='';
-=======
-  vecinosArray:Vecino;
-  vecinosFilter:Vecino;
-  vecinosClave:Array<Vecino>=[];
-  InputEmail:string='';
-  Password:string='';
->>>>>>> ab95ec40a006e5291b2ec4bf5df31f7503194ebb
+  InputEmail: string = '';
+  Password: string = '';
 
-  ngOnInit() {
-  }
+  // vecinosArray: Vecino;
+  // vecinosFilter: Vecino;
+  // vecinosClave: Array<Vecino> = [];
+  // InputEmail: string = '';
+  // Password: string = '';
+
+
+  // ngOnInit() {
+  // }
   // loginUser(e) {
   // 	e.preventDefault();
   // 	console.log(e);
   // 	var username = e.target.elements[0].value;
   // 	var password = e.target.elements[1].value;
-  	
+
   // 	if(username == 'admin' && password == 'admin') {
   //     this.user.setUserLoggedIn(username);
   // 		this.router.navigate(['menu']);
   // 	}
   // }
 
-  
-  buscarVecino()
-  {
 
-    this.vecino.getVecinoEmail(this.InputEmail).subscribe(email => {
-      this.vecinosArray = email;
-      console.log(this.vecinosArray);
-      
-    });
-  }
+  // buscarVecino()
+  // {
 
+  //   this.Vecino.getVecinoEmail(this.InputEmail).subscribe(email => {
+  //     this.vecinosArray = email;
+  //     console.log(this.vecinosArray);
 
-=======
+  //   });
+  // }
+
   constructor(
     private fb: FormBuilder,
     private authService: AuthService
@@ -74,7 +76,7 @@ export class LoginComponent implements OnInit {
       (this.form.get(field).untouched && this.formSubmitAttempt)
     );
   }
->>>>>>> 1cabd71243343faa5da3b11351564319ee7bf778
+
 
   onSubmit() {
     if (this.form.valid) {
