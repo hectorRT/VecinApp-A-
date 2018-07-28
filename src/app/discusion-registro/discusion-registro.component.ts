28ad2c767a0e09a435805edd586a40137eddc372
EstebanMenaUCNE <esteban_mena@ucne.edu.do>
2018-07-28 02:48:46 -0400

    Manejo de sesión en registro de discusiones: Agregué idvecindario a las discusiones

--- src/app/discusion-registro/discusion-registro.component.ts
+++ src/app/discusion-registro/discusion-registro.component.ts
@@ -5,6 +5,9 @@ import { EstadoDiscusion } from '../../Clases/EstadoDiscusion';
 import { FormControl } from '../../../node_modules/@angular/forms';
 import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
+import { AuthenticationService } from './../_services/authentication.service';
+import { Vecino } from '../../Clases/Vecino';
+
 @Component({
   selector: 'app-discusion-registro',
   templateUrl: './discusion-registro.component.html',
@@ -14,12 +17,28 @@ export class DiscusionRegistroComponent implements OnInit {
 
   discusion: Discusion = {};
   estados: Array<EstadoDiscusion> = [];
+  idVecino: number = 0;
+  idVecindario: number = 0;
 
-  constructor(private discusionService: DiscusionesService, private _route: ActivatedRoute) {
+  constructor(private discusionService: DiscusionesService, private _route: ActivatedRoute, public auth: AuthenticationService) {
+    
   }
 
+  getCurrentUser() {
+    if (localStorage.getItem('token')) {
+      this.auth.ObtenerDatos(localStorage.getItem('token')).subscribe(resultado => {
+          this.idVecindario = resultado[0].IdVecindario;
+          this.idVecino = resultado[0].IdVecino;
+          console.log("IdVecindario: " + this.idVecindario + " - IdVecino: " + this.idVecino);
+      })
+    } else {
+        this.auth.authenticated = false;
+        localStorage.removeItem('token');
+    }
+  }
   
   ngOnInit() {
+    this.getCurrentUser();
     this.fetchEstados();
     
     let id: number = +this._route.snapshot.paramMap.get('id');
@@ -56,8 +75,8 @@ export class DiscusionRegistroComponent implements OnInit {
   
   create(){
     this.discusion.DateModification = this.discusion.FechaCreacion;
-    this.discusion.IdVecino = 1;
-    this.discusion.ModifyBy = 1;
+    this.discusion.IdVecino = this.idVecindario;//Hubo un fallo, el idvecino de las discusiones será el id vecindario
+    this.discusion.ModifyBy = this.idVecino;
 
     this.discusionService.addDiscusion(this.discusion).subscribe(res =>
       {
